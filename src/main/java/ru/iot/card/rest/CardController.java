3491feefd56b888d524f97205ee7b43b9e3e7c83
Dmitrii Shevchuk <dshevchuk@phoenixit.ru>
2024-05-13 19:46:59 +0300

    Small fixes

--- src/main/java/ru/iot/card/rest/CardController.java
+++ src/main/java/ru/iot/card/rest/CardController.java
@@ -1,6 +1,7 @@
 package ru.iot.card.rest;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +11,7 @@ import ru.iot.card.dto.BalanceDto;
 import ru.iot.card.dto.CardDto;
 import ru.iot.card.service.CardService;
 
+@Slf4j
 @RestController
 @RequestMapping("cards")
 @RequiredArgsConstructor
@@ -19,12 +21,14 @@ public class CardController {
 
     @PostMapping
     private void create(@RequestBody CardDto cardDto) {
+        log.info("Привязка новой карты {}", cardDto);
         cardService.save(cardDto);
     }
 
 
     @PutMapping
     private void update(@RequestBody BalanceDto balanceDto) {
+        log.info("Обновление баланса {}", balanceDto);
         cardService.update(balanceDto);
     }
 
